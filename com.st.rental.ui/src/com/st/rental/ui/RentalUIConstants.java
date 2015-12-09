3bf5e67e48b8bb577d36405b07d9882521942bfe
localadmin <localadmin@localadmin-UX32VD>
2015-12-09 11:09:25 +0100

    RentalAgency view: display complete tree
    > Agency
    	> Customers
    		> John Wayne
    		> ...
    	> Objects To Rent
    		> Perceuse
    		> ...
    	> Rentals
    		> Perceuse loué par John Wayne du 10/12 au 25/12
    		> ...
    
    Change-Id: I95b35fa3d8418e569d96b9bd294f6a5fb06194e1
    Signed-off-by: localadmin <localadmin@localadmin-UX32VD>

---
+++ com.st.rental.ui/src/com/st/rental/ui/RentalUIConstants.java
@@ -0,0 +1,7 @@
+package com.st.rental.ui;
+
+public interface RentalUIConstants {
+	public static final String NODE_CUSTOMERS = "Customers";
+	public static final String NODE_RENTAL_OBJECTS = "Objects To Rent";
+	public static final String NODE_RENTALS = "Rentals";
+}
--- com.st.rental.ui/src/com/st/rental/ui/views/RentalProvider.java
+++ com.st.rental.ui/src/com/st/rental/ui/views/RentalProvider.java
@@ -7,7 +7,10 @@ import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.jface.viewers.Viewer;
 
 import com.opcoach.training.rental.Customer;
+import com.opcoach.training.rental.Rental;
 import com.opcoach.training.rental.RentalAgency;
+import com.opcoach.training.rental.RentalObject;
+import com.st.rental.ui.RentalUIConstants;
 
 public class RentalProvider extends LabelProvider implements ITreeContentProvider {
 
@@ -32,7 +35,14 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 
 		if (parentElement instanceof RentalAgency) {
 			RentalAgency agency = (RentalAgency)parentElement;
-			elements = agency.getCustomers().toArray();
+			Node[] nodes = new Node[3];
+			nodes[0] = new Node(RentalUIConstants.NODE_CUSTOMERS, agency);
+			nodes[1] = new Node(RentalUIConstants.NODE_RENTAL_OBJECTS, agency);
+			nodes[2] = new Node(RentalUIConstants.NODE_RENTALS, agency);
+			elements = nodes;
+		}
+		else if (parentElement instanceof Node) {
+			return ((Node) parentElement).getChildren();
 		}
 
 		return elements;
@@ -49,6 +59,9 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		if (element instanceof RentalAgency) {
 			return true;
 		}
+		else if (element instanceof Node) {
+			return true;
+		}
 
 		return false;
 	}
@@ -62,7 +75,44 @@ public class RentalProvider extends LabelProvider implements ITreeContentProvide
 		else if (element instanceof Customer) {
 			return ((Customer) element).getDisplayName();
 		}
+		else if (element instanceof RentalObject) {
+			return ((RentalObject) element).getName();
+		}
+		else if (element instanceof Rental) {
+			return ((Rental) element).toString();
+		}
 
 		return super.getText(element);
+	}
+
+
+	class Node {
+		private String title;
+		private RentalAgency agency;
+
+		public Node(String title, RentalAgency agency) {
+			super();
+			this.title = title;
+			this.agency = agency;
+		}
+		
+		public Object[] getChildren() {
+			if (title == RentalUIConstants.NODE_CUSTOMERS) {
+				return agency.getCustomers().toArray();
+			}
+			else if (title == RentalUIConstants.NODE_RENTAL_OBJECTS) {
+				return agency.getObjectsToRent().toArray();
+			}
+			else if (title == RentalUIConstants.NODE_RENTALS) {
+				return agency.getRentals().toArray();
+			}
+
+			return null;
+		}
+		
+		@Override
+		public String toString() {
+			return title;
+		}
 	}
 }
