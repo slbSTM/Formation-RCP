a6661db5f39adcf7afb3072facc685d90cff2b61
localadmin <localadmin@localadmin-UX32VD>
2015-12-09 16:13:23 +0100

    Add Rental Perspective containing all Rental views
    Change-Id: Ic4aaad3120169dd6379eb4a95d5790718743ae43
    Signed-off-by: localadmin <localadmin@localadmin-UX32VD>

---
+++ com.st.rental.ui/src/com/st/rental/ui/perspectives/RentalPerspective.java
@@ -0,0 +1,40 @@
+package com.st.rental.ui.perspectives;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+import org.eclipse.ui.IFolderLayout;
+
+public class RentalPerspective implements IPerspectiveFactory {
+
+	/**
+	 * Creates the initial layout for a page.
+	 */
+	public void createInitialLayout(IPageLayout layout) {
+		layout.setEditorAreaVisible(false);
+		String editorArea = layout.getEditorArea();
+		addFastViews(layout);
+		addViewShortcuts(layout);
+		addPerspectiveShortcuts(layout);
+		layout.addView("com.st.rental.ui.view.rental.agency", IPageLayout.LEFT, 0.7f, IPageLayout.ID_EDITOR_AREA);
+		layout.addView("com.st.rental.ui.view.rental.property", IPageLayout.RIGHT, 0.5f, "com.st.rental.ui.view.rental.agency");
+	}
+
+	/**
+	 * Add fast views to the perspective.
+	 */
+	private void addFastViews(IPageLayout layout) {
+	}
+
+	/**
+	 * Add view shortcuts to the perspective.
+	 */
+	private void addViewShortcuts(IPageLayout layout) {
+	}
+
+	/**
+	 * Add perspective shortcuts to the perspective.
+	 */
+	private void addPerspectiveShortcuts(IPageLayout layout) {
+	}
+
+}
